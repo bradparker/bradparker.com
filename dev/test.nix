8ece4c98bf705b498d44b64f7c9e1e78e72eccf1
Brad Parker <hi@bradparker.com>
2019-11-04 20:28:00 +1000

    A better module, sets up acme automatically

--- dev/test.nix
+++ dev/test.nix
@@ -1,27 +1,12 @@
 _:
-let
-  acmeWebRoot = "/var/www/acme";
-  acmeKeyDir = "/var/lib/acme";
-in
   {
     imports = [ ../module.nix ];
 
-    security.acme.certs = {
-      "bradparker.com" = {
-        email = "hi@bradparker.com";
-        webroot = "${acmeWebRoot}";
-        postRun = "systemctl reload bradparker-com.service";
-      };
-    };
-
     services = {
-      bradparker-com = {
+      "bradparker.com" = {
         enable = true;
         port = 443;
-        https = {
-          keyFile = "${acmeKeyDir}/bradpaker.com/key.pem";
-          certFile = "${acmeKeyDir}/bradparker.com/fullchain.pem";
-        };
+        https.enable = true;
       };
     };
   }
--- module.nix
+++ module.nix
@@ -3,58 +3,63 @@ let
   server = import ./server;
   site = import ./.;
 
-  serviceConfig = config.services.bradparker-com;
+  serverName = "bradparker.com";
+  serviceConfig = config.services."${serverName}";
   options = {
-    enable = lib.mkEnableOption "bradparker.com service";
+    enable = lib.mkEnableOption "${serverName} service";
     port = lib.mkOption {
       type = lib.types.port;
       description = "Port number.";
       default = 80;
     };
     https = lib.mkOption {
-      type = lib.types.nullOr (lib.types.submodule {
+      type = lib.types.submodule {
         options = {
-          certFile = lib.mkOption {
-            type = lib.types.string;
-            description = "Absolute path to certfile";
-            default = "";
-          };
-          keyFile = lib.mkOption {
-            type = lib.types.string;
-            description = "Absolute path to keyfile";
-            default = "";
+          enable = lib.mkEnableOption "HTTPS";
+          acmeWebRoot = lib.mkOption {
+            type = lib.types.str;
+            description = "Directory for the acme challenge which is PUBLIC, don't put certs or keys in here";
+            default = "/var/lib/acme/acme-challenge";
           };
         };
-      });
-      description = "Config required for HTTPS";
-      default = null;
+      };
+      description = "HTTPS config";
+      default = { enable = false; };
     };
   };
 
   args = with serviceConfig; [
     "--port ${toString port}"
     "--directory ${site}"
-  ] ++ lib.optionals (https != null) [
-    "--keyFile ${https.keyFile}"
-    "--certFile ${https.certFile}"
+  ] ++ lib.optionals https.enable [
+    "--certFile ${config.security.acme.directory}/${serverName}/fullchain.pem"
+    "--keyFile ${config.security.acme.directory}/${serverName}/key.pem"
   ];
 in
   {
-    options.services.bradparker-com = options;
+    options.services.${serverName} = options;
     config = lib.mkIf serviceConfig.enable {
-      systemd.services.bradparker-com = {
+      systemd.services.${serverName} = {
         wantedBy = [ "multi-user.target" ];
         script = ''
           ${server}/bin/server ${lib.concatStringsSep " " args}
         '';
         description = ''
-          https://bradparker.com
+          https://${serverName}
         '';
         serviceConfig = {
           KillSignal="INT";
           Type = "simple";
           Restart = "on-abort";
           RestartSec = "10";
+        };
+      };
+
+      security.acme.certs = {
+        ${serverName} = lib.mkIf serviceConfig.https.enable {
+          email = "hi@bradparker.com";
+          webroot = "${serviceConfig.https.acmeWebRoot}";
+          postRun = "systemctl reload ${serverName}.service";
         };
       };
     };
