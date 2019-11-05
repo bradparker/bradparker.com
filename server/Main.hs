5a0fdd44a2cabcfd873ec822b2ee7121c636d0f7
Brad Parker <hi@bradparker.com>
2019-11-05 21:41:18 +1000

    Ormolu the server, tidy up its cabal file

--- server/Main.hs
+++ server/Main.hs
@@ -2,82 +2,92 @@
 
 module Main
   ( main
-  )
+    )
 where
 
 import Control.Applicative (optional)
 import Network.Wai (Application)
 import Network.Wai.Application.Static
-  ( StaticSettings(ssIndices)
-  , defaultWebAppSettings
-  , staticApp
-  )
+  ( StaticSettings (ssIndices),
+    defaultWebAppSettings,
+    staticApp
+    )
 import qualified Network.Wai.Handler.Warp as Warp
 import Network.Wai.Handler.Warp (Port)
 import qualified Network.Wai.Handler.WarpTLS as WarpTLS
 import Options.Applicative
-  ( Parser
-  , auto
-  , execParser
-  , fullDesc
-  , help
-  , info
-  , long
-  , metavar
-  , option
-  , strOption
-  )
+  ( Parser,
+    auto,
+    execParser,
+    fullDesc,
+    help,
+    info,
+    long,
+    metavar,
+    option,
+    strOption
+    )
 import WaiAppStatic.Types (unsafeToPiece)
 
-data HttpsOptions = HttpsOptions
-  { keyFile :: String
-  , certFile :: String
-  }
+data HttpsOptions
+  = HttpsOptions
+      { keyFile :: String,
+        certFile :: String
+        }
 
-data Options = Options
-  { port :: Port
-  , directory :: FilePath
-  , https :: Maybe HttpsOptions
-  }
+data Options
+  = Options
+      { port :: Port,
+        directory :: FilePath,
+        https :: Maybe HttpsOptions
+        }
 
 optionsP :: Parser Options
 optionsP =
   Options <$> portP <*> directoryP <*> optional httpsP
- where
-  portP =
-    option auto (long "port" <> metavar "PORT")
-  directoryP =
-    strOption (long "directory" <> metavar "DIRECTORY")
-  httpsP =
-    HttpsOptions
-      <$> strOption
-            (  long "https-key-file"
-            <> metavar "HTTPS_KEY_FILE"
-            )
-      <*> strOption
-            (  long "https-cert-file"
-            <> metavar "HTTPS_CERT_FILE"
-            )
+  where
+    portP = option auto (long "port" <> metavar "PORT")
+    directoryP = strOption (long "directory" <> metavar "DIRECTORY")
+    httpsP =
+      HttpsOptions
+        <$> strOption
+              ( long "https-key-file"
+                  <> metavar "HTTPS_KEY_FILE"
+                )
+        <*> strOption
+              ( long "https-cert-file"
+                  <> metavar "HTTPS_CERT_FILE"
+                )
 
 app :: FilePath -> Application
 app path = staticApp settings
- where
-  defaults = defaultWebAppSettings path
-  index    = unsafeToPiece "index.html"
-  settings = defaults { ssIndices = [index] }
+  where
+    defaults = defaultWebAppSettings path
+    index = unsafeToPiece "index.html"
+    settings = defaults {ssIndices = [index]}
+
+tlsSettings :: HttpsOptions -> WarpTLS.TLSSettings
+tlsSettings httpsOpts =
+  WarpTLS.defaultTlsSettings
+    { WarpTLS.certFile = certFile httpsOpts,
+      WarpTLS.keyFile = keyFile httpsOpts
+      }
+
+runTLS :: HttpsOptions -> Warp.Settings -> Application -> IO ()
+runTLS httpsOpts = WarpTLS.runTLS (tlsSettings httpsOpts)
+
+settings :: Options -> Warp.Settings
+settings options = Warp.setPort (port options) Warp.defaultSettings
+
+run :: Options -> Application -> IO ()
+run options =
+  maybe
+    Warp.runSettings
+    runTLS
+    (https options)
+    (settings options)
 
 main :: IO ()
 main = do
   options <- execParser $ info optionsP fullDesc
-  let settings =
-        Warp.setPort (port options) Warp.defaultSettings
-  case https options of
-    Nothing ->
-      Warp.runSettings settings $ app (directory options)
-    Just httpsOpts -> do
-      let tlsSettings = WarpTLS.defaultTlsSettings
-            { WarpTLS.certFile = certFile httpsOpts
-            , WarpTLS.keyFile  = keyFile httpsOpts
-            }
-      WarpTLS.runTLS tlsSettings settings
-        $ app (directory options)
+  run options $ app $ directory options
