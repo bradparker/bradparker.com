4052410d42b4f86914e23efe9beedb1cf0c5ffc7
Brad Parker <hi@bradparker.com>
2019-11-17 12:59:53 +1000

    Better organize default.nix

--- default.nix
+++ default.nix
@@ -1,21 +1,23 @@
 let
   nixpkgs = import ./nixpkgs.nix;
-  builder = import ./builder;
-  bradparker-com = nixpkgs.stdenv.mkDerivation {
-    name = "bradparker-com";
-    src = ./.;
-    buildInputs = [ nixpkgs.glibcLocales ];
-    buildPhase = ''
-      export LOCALE_ARCHIVE="${nixpkgs.glibcLocales}/lib/locale/locale-archive"
-      export LANG="en_AU.UTF-8";
-      export LC_TYPE="en_AU.UTF-8";
+in {
+  bradparker-com = rec {
+    builder = import ./builder;
+    site = nixpkgs.stdenv.mkDerivation {
+      name = "bradparker-com-site";
+      src = ./.;
+      buildInputs = [ nixpkgs.glibcLocales ];
+      buildPhase = ''
+        export LOCALE_ARCHIVE="${nixpkgs.glibcLocales}/lib/locale/locale-archive"
+        export LANG="en_AU.UTF-8";
+        export LC_TYPE="en_AU.UTF-8";
 
-      ${builder}/bin/builder build
-    '';
-    installPhase = ''
-      mkdir -p $out
-      cp -R _site/* $out/var/www/bradparker.com/
-    '';
+        ${builder}/bin/builder build
+      '';
+      installPhase = ''
+        mkdir -p $out/var/www/bradparker.com/
+        cp -R _site/* $out/var/www/bradparker.com/
+      '';
+    };
   };
-in
-  { bradparker-com = bradparker-com; }
+}
