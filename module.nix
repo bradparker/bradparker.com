3e55fd416a5ad57372d08c8ca0e21c667270ee43
Brad Parker <hi@bradparker.com>
2019-11-30 15:08:10 +1000

    Rearrange module service order

--- module.nix
+++ module.nix
@@ -15,7 +15,27 @@ let
 in
   {
     options.services.${serverName} = options;
+
     config = lib.mkIf serviceConfig.enable {
+      systemd.services."source-${serverName}" = {
+        description = ''
+          https://${serverName} source
+        '';
+        serviceConfig = {
+          Type = "oneshot";
+        };
+        startAt = "*:0/5";
+        path = with pkgs; [ nix gnutar gzip curl jq ];
+        script = ''
+          set -ex
+
+          rev=$(curl https://api.github.com/repos/bradparker/bradparker.com/git/ref/heads/source | jq -r .object.sha)
+          result=$(nix-build https://github.com/bradparker/bradparker.com/archive/$rev.tar.gz -A bradparker-com.site)
+
+          ln -sfT $result${webRoot} ${webRoot}
+        '';
+      };
+
       systemd.services.${serverName} = {
         wantedBy = [ "multi-user.target" ];
         wants = [
@@ -40,25 +60,6 @@ in
           Restart = "on-abort";
           RestartSec = "10";
         };
-      };
-
-      systemd.services."source-${serverName}" = {
-        description = ''
-          https://${serverName} source
-        '';
-        serviceConfig = {
-          Type = "oneshot";
-        };
-        startAt = "*:0/5";
-        path = with pkgs; [ nix gnutar gzip curl jq ];
-        script = ''
-          set -ex
-
-          rev=$(curl https://api.github.com/repos/bradparker/bradparker.com/git/ref/heads/source | jq -r .object.sha)
-          result=$(nix-build https://github.com/bradparker/bradparker.com/archive/$rev.tar.gz -A bradparker-com.site)
-
-          ln -sfT $result${webRoot} ${webRoot}
-        '';
       };
 
       systemd.services."acme-challenge-${serverName}" = {
