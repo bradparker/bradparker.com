1d1b599de3ffc6b91a06ff824b60676e7af64809
Brad Parker <hi@bradparker.com>
2022-11-05 00:45:43 +1000

    Don't haddock, to memory hungry

--- bradparker.com/usr/local/src/nixpkgs.nix
+++ bradparker.com/usr/local/src/nixpkgs.nix
@@ -4,9 +4,9 @@ let
   haskellPackagesOverlay = self: super: {
     haskellPackages = super.haskellPackages.override {
       overrides = (hself: hsuper: {
-        pandoc-lens = hself.callCabal2nix "pandoc-lens" sources.pandoc-lens {};
-        wai-cli = hself.callCabal2nix "wai-cli" sources.wai-cli {};
-        socket-activation = hself.callCabal2nix "socket-activation" sources.haskell-socket-activation {};
+        pandoc-lens = super.haskell.lib.dontHaddock (hself.callCabal2nix "pandoc-lens" sources.pandoc-lens {});
+        wai-cli = super.haskell.lib.dontHaddock (hself.callCabal2nix "wai-cli" sources.wai-cli {});
+        socket-activation = super.haskell.lib.dontHaddock (hself.callCabal2nix "socket-activation" sources.haskell-socket-activation {});
       });
     };
   };
