ef525a25e0c546c0cf917147b1d1957239a7e56b
Brad Parker <hi@bradparker.com>
2022-11-05 16:13:22 +1000

    Remove now unnecessary dep pins

--- bradparker.com/usr/local/src/nixpkgs.nix
+++ bradparker.com/usr/local/src/nixpkgs.nix
@@ -5,8 +5,6 @@ let
     haskellPackages = super.haskellPackages.override {
       overrides = (hself: hsuper: {
         pandoc-lens = super.haskell.lib.dontHaddock (hself.callCabal2nix "pandoc-lens" sources.pandoc-lens {});
-        wai-cli = super.haskell.lib.dontHaddock (hself.callCabal2nix "wai-cli" sources.wai-cli {});
-        socket-activation = super.haskell.lib.dontHaddock (hself.callCabal2nix "socket-activation" sources.haskell-socket-activation {});
       });
     };
   };
