18d7ced9e603427eb15d09846bc8a57398b4876d
Brad Parker <hi@bradparker.com>
2023-02-12 13:24:02 +1000

    Add Starbook SSH Key

--- bradparker.com/etc/nixos/configuration.nix
+++ bradparker.com/etc/nixos/configuration.nix
@@ -33,7 +33,11 @@
       "wheel"
     ];
     openssh.authorizedKeys.keys = [
+      # FGF MacBook
       "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAACAQCiv/ll8r72qnuI6pWpFkt9NEiMvYD6nEvAZZhEMKqQ3Ad3GRnfG5x1aWmuxdk2+DcR6OaLVa7uZ9IwrDk2loHWneSFQOH7GvMETOou2JMZ/0YIl7e3ATjBzZGaux5makK5eift/TuEsHohK7sP9cnUu/plc+pxPhxlq6yQi61dAFKUPDU8Y/SsFwzzkuIscO7pM9ohr2Ddukju5k94OyCRhsJnVYucODF0p5TnzBejzwS/9xmRfoJmQwPGn60IyvAMWBxL3VJ+uFvNUUj+uUXj68LXEnT7X4NfLlrRC9GC5wT8j5ehgEQ9L3EYsVDV8p4dRE7lEBoS9obWH+dm6s1ngkEGT+J5c9B9foaNPsTwEVyhiXGmRHMGDm8zxhZOVIuOACzGWSjO6Lx3lKDg6v4CnV5UfSJtj17FwhubnIK+cwTD6jB0GoNZplTG4tOsyxVvk787JJ7uZeo8sbaKl5OtgmNF2lJ3XVx+UhnRm3xpPG1XsGQKkFapb1apGQ869hwRzkhpMfM31LO8HQ4MdF2TQ6duR6/jMlhdWmhQAcsWhIW8c8JhwRvQgcLoiZziBheqXqRu5u9yltoMP2vC1d/ofLtWA3yugCUP5QLzt4E4IjJHSEFlWSYugJjpcg/3fm+JuXjIklHtstRvz9G1001TuzSb5rAGQ7dj8eBqyvUKUQ== brad.parker@FGFMAC66.localdomain"
+
+      # Starbook
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOYaoaPFRbC3cUYqpWmKErINBXl7c6/l1LFz0r7QlCct hi@bradparker.com"
     ];
   };
 
