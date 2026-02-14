49e140cc0e8b98c9d9b978982b2b6180f07b80cb
Brad Parker <hi@bradparker.com>
2026-02-14 22:29:03 +1000

    Expand everything out, more explicit, correct errors

--- bradparker.com/usr/local/src/bradparker.com/site/content/notes/2025-09-24-concentrations.md
+++ bradparker.com/usr/local/src/bradparker.com/site/content/notes/2025-09-24-concentrations.md
@@ -1,5 +1,5 @@
 ---
-title: Concentrations
+title: "Concentrations: algebraically"
 tags:
   - Mathematics
 description: |
@@ -22,72 +22,329 @@ c_a v_a + c_b (v - v_a) = c v
 \end{multline}
 $$
 
-Each of these can be used to derive other, more obviously useful, I think, equations.
+Each of these can be used to derive expressions for roots. Useful for finding expressions for each variable.
 
 $$
 \begin{multline}
 c_a v_a + c_b v_b = c (v_a + v_b) \\
-\iff v_a (c_a - c) + v_b (c_b + c) = 0
+\iff c_a v_a + c_b v_b - c (v_a + v_b) = 0 \\
+\iff v_a (c_a - c) + v_b (c_b - c) = 0
 \end{multline}
 $$
+
+<details>
+  <summary>
+    Proof
+  </summary>
+
+  <div class="p-4 rounded-lg ba b--light-gray markdown">
+
+  $$
+  \begin{multline}
+  c_a v_a + c_b v_b = c (v_a + v_b) \\
+  \iff c_a v_a + c_b v_b - c (v_a + v_b) = 0 \\
+  \iff c_a v_a + c_b v_b - c v_a - c v_b = 0 \\
+  \iff v_a (c_a - c) + v_b (c_b - c) = 0 \\
+  \end{multline}
+  $$
+
+  </div>
+</details>
 
 $$
 \begin{multline}
 c_a (v - v_b) + c_b v_b = c v \\
-\iff v (c_a - c) - v_b (c_a + c_b) = 0
+\iff c_a (v - v_b) + c_b v_b - c v = 0 \\
+\iff v (c_a - c) + v_b (c_b - c_a)  = 0
 \end{multline}
 $$
+
+<details>
+  <summary>
+    Proof
+  </summary>
+
+  <div class="p-4 rounded-lg ba b--light-gray markdown">
+
+  $$
+  \begin{multline}
+  c_a (v - v_b) + c_b v_b = c v \\
+  \iff c_a (v - v_b) + c_b v_b - c v = 0 \\
+  \iff c_a v - c_a v_b + c_b v_b - c v = 0 \\
+  \iff v (c_a - c) + v_b (c_b - c_a)  = 0
+  \end{multline}
+  $$
+
+  </div>
+</details>
 
 $$
 \begin{multline}
 c_a v_a + c_b (v - v_a) = c v \\
+\iff c_a v_a + c_b (v - v_a) - c v = 0 \\
 \iff v_a (c_a - c_b) + v (c_b - c) = 0
 \end{multline}
 $$
 
+<details>
+  <summary>
+    Proof
+  </summary>
+
+  <div class="p-4 rounded-lg ba b--light-gray markdown">
+
+  $$
+  \begin{multline}
+  c_a v_a + c_b (v - v_a) = c v \\
+  \iff c_a v_a + c_b (v - v_a) - c v = 0 \\
+  \iff c_a v_a + c_b v - c_b v_a - c v = 0 \\
+  \iff v_a (c_a - c_b) + v (c_b - c) = 0 \\
+  \end{multline}
+  $$
+
+  </div>
+</details>
+
 From here I can enumerate all the possible equations for each variable.
 
-## In terms of _c_
+## Concentrations
+
+### In terms of _c_
 
 $$
 c = \frac{c_a v_a + c_b v_b}{v_a + v_b}
 $$
 
+<details>
+  <summary>
+    Proof
+  </summary>
+
+  <div class="p-4 rounded-lg ba b--light-gray markdown">
+
+  $$
+  \begin{multline}
+  c_a v_a + c_b v_b - c (v_a + v_b) = 0 \\
+  \iff c_a v_a + c_b v_b = c (v_a + v_b) \\
+  \iff \frac{c_a v_a + c_b v_b}{(v_a + v_b)} = c \\
+  \end{multline}
+  $$
+
+  </div>
+</details>
+
 $$
 c = \frac{c_a (v - v_b) + c_b v_b}{v}
 $$
+
+<details>
+  <summary>
+    Proof
+  </summary>
+
+  <div class="p-4 rounded-lg ba b--light-gray markdown">
+
+  $$
+  \begin{multline}
+  c_a (v - v_b) + c_b v_b - c v = 0 \\
+  \iff c_a (v - v_b) + c_b v_b = c v \\
+  \iff \frac{c_a (v - v_b) + c_b v_b}{v} = c \\
+  \end{multline}
+  $$
+
+  </div>
+</details>
 
 $$
 c = \frac{c_a v_a + c_b (v - v_a)}{v}
 $$
 
-## In terms of _v_
+Commutativity means the prior expression is proved equivalently.
+
+### In terms of _c<sub>a</sub>_ / _c<sub>b</sub>_
+
+$$
+c_a = \frac{c (v_a + v_b) - c_b v_b}{v_a}
+$$
+
+<details>
+  <summary>
+    Proof
+  </summary>
+
+  <div class="p-4 rounded-lg ba b--light-gray markdown">
+
+  $$
+  \begin{multline}
+  c_a v_a + c_b v_b - c (v_a + v_b) = 0 \\
+  \iff c_b v_b - c (v_a + v_b) = -c_a v_a \\
+  \iff \frac{c_b v_b - c (v_a + v_b)}{v_a} = -c_a \\
+  \iff \frac{-(c_b v_b - c (v_a + v_b))}{v_a} = c_a \\
+  \iff \frac{-c_b v_b + c (v_a + v_b)}{v_a} = c_a \\
+  \iff \frac{c (v_a + v_b) - c_b v_b}{v_a} = c_a \\
+  \end{multline}
+  $$
+
+  </div>
+</details>
+
+$$
+c_a = \frac{c v - c_b (v - v_a)}{v_a}
+$$
+
+<details>
+  <summary>
+    Proof
+  </summary>
+
+  <div class="p-4 rounded-lg ba b--light-gray markdown">
+
+  $$
+  \begin{multline}
+  c_a v_a + c_b (v - v_a) - c v = 0 \\
+  \iff c_b (v - v_a) - c v = -c_a v_a  \\
+  \iff \frac{c_b (v - v_a) - c v}{v_a} = -c_a  \\
+  \iff \frac{-(c_b (v - v_a) - c v)}{v_a} = c_a  \\
+  \iff \frac{-c_b (v - v_a) + c v}{v_a} = c_a  \\
+  \iff \frac{c v - c_b (v - v_a)}{v_a} = c_a  \\
+  \end{multline}
+  $$
+
+  </div>
+</details>
+
+$$
+c_a = \frac{c v - c_b v_b}{v - v_b}
+$$
+
+<details>
+  <summary>
+    Proof
+  </summary>
+
+  <div class="p-4 rounded-lg ba b--light-gray markdown">
+
+  $$
+  \begin{multline}
+  c_a (v - v_b) + c_b v_b - c v = 0 \\
+  \iff c_b v_b - c v = -c_a (v - v_b)  \\
+  \iff \frac{c_b v_b - c v}{v - v_b} = -c_a  \\
+  \iff \frac{-(c_b v_b - c v)}{v - v_b} = c_a  \\
+  \iff \frac{-c_b v_b + c v)}{v - v_b} = c_a  \\
+  \iff \frac{c v - c_b v_b)}{v - v_b} = c_a  \\
+  \end{multline}
+  $$
+
+  </div>
+</details>
+
+Commutativity means each of the next three expressions is proved equivalently to its companion among the prior three.
+
+$$
+c_b = \frac{c (v_b + v_a) - c_a v_a}{v_b}
+$$
+
+$$
+c_b = \frac{c v - c_a (v - v_b)}{v_b}
+$$
+
+$$
+c_b = \frac{c v - c_a v_a}{v - v_a}
+$$
+
+## Volumes
+
+### In terms of _v_
 
 $$
 v = v_a + v_b
 $$
 
+By definition.
+
 $$
 v = \frac{v_a(c_b - c_a)}{c_b - c}
 $$
+
+<details>
+  <summary>
+    Proof
+  </summary>
+
+  <div class="p-4 rounded-lg ba b--light-gray markdown">
+
+  $$
+  \begin{multline}
+  v_a (c_a - c_b) + v (c_b - c) = 0 \\
+  \iff v_a (c_a - c_b) = v (c_b - c) \\
+  \iff \frac{v_a (c_a - c_b)}{c_b - c} = v \\
+  \end{multline}
+  $$
+
+  </div>
+</details>
 
 $$
 v = \frac{v_b(c_a - c_b)}{c_a - c}
 $$
 
-## In terms of _v<sub>a</sub>_ / _v<sub>b</sub>_
+Commutativity means this is proved equivalently to the prior expression.
+
+### In terms of _v<sub>a</sub>_ / _v<sub>b</sub>_
 
 $$
 v_a = v - v_b
 $$
 
+By definition of $v$ as $v_a + v_b$.
+
 $$
 v_a = \frac{v_b (c_b - c)}{c - c_a}
 $$
 
+<details>
+  <summary>
+    Proof
+  </summary>
+
+  <div class="p-4 rounded-lg ba b--light-gray markdown">
+
+  $$
+  \begin{multline}
+  v_a (c_a - c) + v_b (c_b - c) = 0 \\
+  \iff v_a (c_a - c) = -v_b (c_b - c) \\
+  \iff v_a = \frac{-v_b (c_b - c)}{c_a - c} \\
+  \iff v_a = \frac{v_b (c_b - c)}{c - c_a} \\
+  \end{multline}
+  $$
+
+  </div>
+</details>
+
 $$
 v_a = \frac{v (c_b - c)}{c_b - c_a}
 $$
+
+<details>
+  <summary>
+    Proof
+  </summary>
+
+  <div class="p-4 rounded-lg ba b--light-gray markdown">
+
+  $$
+  \begin{multline}
+  v_a (c_a - c_b) + v (c_b - c) = 0 \\
+  \iff v_a (c_a - c_b) = -v (c_b - c) \\
+  \iff v_a = \frac{-v (c_b - c)}{c_a - c_b} \\
+  \iff v_a = \frac{v (c_b - c)}{c_b - c_a} \\
+  \end{multline}
+  $$
+
+  </div>
+</details>
+
+Commutativity means each of the next three expressions is proved equivalently to its companion among the prior three.
 
 $$
 v_b = v - v_a
@@ -101,28 +358,4 @@ $$
 v_b = \frac{v (c_a - c)}{c_a - c_b}
 $$
 
-## In terms of _c<sub>a</sub>_ / _c<sub>b</sub>_
 
-$$
-c_a = \frac{c (v_a + v_b) - c_b v_b}{v_a}
-$$
-
-$$
-c_a = \frac{c v - c_b (v - v_a)}{v_a}
-$$
-
-$$
-c_a = \frac{c v - c_b v_b}{v - v_b}
-$$
-
-$$
-c_b = \frac{c (v_b + v_a) - c_a v_a}{v_b}
-$$
-
-$$
-c_b = \frac{c v - c_a (v - v_b)}{v_b}
-$$
-
-$$
-c_b = \frac{c v - c_a v_a}{v - v_a}
-$$
--- bradparker.com/usr/local/src/bradparker.com/site/content/notes/concentrations-calculator/assets/main.js
+++ bradparker.com/usr/local/src/bradparker.com/site/content/notes/concentrations-calculator/assets/main.js
@@ -28,6 +28,77 @@ const getElementByScopedName = (scope) => (name) =>
 const approxEqual = (a, b) => Math.abs(a - b) <= 0.001;
 
 const completions = {
+  // Concentrations
+  c: [
+    {
+      needs: new Set(["c_a", "v_a", "c_b", "v_b"]),
+      complete({ c_a, v_a, c_b, v_b }) {
+        if (-v_a === v_b) return null;
+        return (c_a * v_a + c_b * v_b) / (v_a + v_b);
+      },
+    },
+    {
+      needs: new Set(["c_a", "v", "v_b", "c_b"]),
+      complete({ c_a, v, v_b, c_b }) {
+        if (v === 0) return null;
+        return (c_a * (v - v_b) + c_b * v_b) / v;
+      },
+    },
+    {
+      needs: new Set(["c_a", "v_a", "c_b", "v"]),
+      complete({ c_a, v_a, c_b, v }) {
+        if (v === 0) return null;
+        return (c_a * v_a + c_b * (v - v_a)) / v;
+      },
+    },
+  ],
+  c_a: [
+    {
+      needs: new Set(["c", "v_a", "v_b", "c_b"]),
+      complete({ c, v_a, v_b, c_b }) {
+        if (v_a === 0) return null;
+        return (c * (v_a + v_b) - c_b * v_b) / v_a;
+      },
+    },
+    {
+      needs: new Set(["c", "v", "c_b", "v_a"]),
+      complete({ c, v, c_b, v_a }) {
+        if (v_a === 0) return null;
+        return (c * v - c_b * (v - v_a)) / v_a;
+      },
+    },
+    {
+      needs: new Set(["c", "v", "c_b", "v_b"]),
+      complete({ c, v, c_b, v_b }) {
+        if (v === v_b) return null;
+        return (c * v - c_b * v_b) / (v - v_b);
+      },
+    },
+  ],
+  c_b: [
+    {
+      needs: new Set(["c", "v_b", "v_a", "c_a"]),
+      complete({ c, v_b, v_a, c_a }) {
+        if (v_b === 0) return null;
+        return (c * (v_b + v_a) - c_a * v_a) / v_b;
+      },
+    },
+    {
+      needs: new Set(["c", "v", "c_a", "v_b"]),
+      complete({ c, v, c_a, v_b }) {
+        if (v_b === 0) return null;
+        return (c * v - c_a * (v - v_b)) / v_b;
+      },
+    },
+    {
+      needs: new Set(["c", "v", "c_a", "v_a"]),
+      complete({ c, v, c_a, v_a }) {
+        if (v === v_a) return null;
+        return (c * v - c_a * v_a) / (v - v_a);
+      },
+    },
+  ],
+  // Volumes
   v: [
     {
       needs: new Set(["v_a", "v_b"]),
@@ -35,22 +106,18 @@ const completions = {
         return v_a + v_b;
       },
     },
-  ],
-  c: [
     {
-      needs: new Set(["c_a", "v_a", "c_b", "v_b", "v"]),
-      complete({ c_a, v_a, c_b, v_b, v }) {
-        if (v === 0) return null;
-        return (c_a * v_a + c_b * v_b) / v;
+      needs: new Set(["v_a", "c_b", "c_a", "c"]),
+      complete({ v_a, c_b, c_a, c }) {
+        if (c_b === c) return null;
+        return (v_a * (c_b - c_a)) / (c_b - c);
       },
     },
-  ],
-  c_a: [
     {
-      needs: new Set(["c", "v", "c_b", "v_b", "v_a"]),
-      complete({ c, v, c_b, v_b, v_a }) {
-        if (v_a === 0) return null;
-        return (c * v - c_b * v_b) / v_a;
+      needs: new Set(["v_b", "c_a", "c_b", "c"]),
+      complete({ v_b, c_a, c_b, c }) {
+        if (c_a === c) return null;
+        return (v_b * (c_a - c_b)) / (c_a - c);
       },
     },
   ],
@@ -65,24 +132,15 @@ const completions = {
     {
       needs: new Set(["v_b", "c_b", "c", "c_a"]),
       complete({ v_b, c_b, c, c_a }) {
-        if (c === c_b) return null;
-        return (-v_b * (c_b - c)) / (c_a - c);
+        if (c === c_a) return null;
+        return (v_b * (c_b - c)) / (c - c_a);
       },
     },
     {
-      needs: new Set(["v", "c", "c_a", "c_b"]),
-      complete({ v, c, c_a, c_b }) {
+      needs: new Set(["v", "c_b", "c", "c_a"]),
+      complete({ v, c_b, c, c_a }) {
         if (c_a === c_b) return null;
-        return (v * (c - c_b)) / (c_a - c_b);
-      },
-    },
-  ],
-  c_b: [
-    {
-      needs: new Set(["c", "v", "c_a", "v_a", "v_b"]),
-      complete({ c, v, c_a, v_a, v_b }) {
-        if (v_b === 0) return null;
-        return (c * v - c_a * v_a) / v_b;
+        return (v * (c_b - c)) / (c_b - c_a);
       },
     },
   ],
@@ -90,26 +148,31 @@ const completions = {
     {
       needs: new Set(["v", "v_a"]),
       complete({ v, v_a }) {
-        if (v < v_a) return null;
         return v - v_a;
       },
     },
     {
       needs: new Set(["v_a", "c_a", "c", "c_b"]),
       complete({ v_a, c_a, c, c_b }) {
-        if (c === c_a) return null;
-        return (-v_a * (c_a - c)) / (c_b - c);
+        if (c === c_b) return null;
+        return (v_a * (c_a - c)) / (c - c_b);
       },
     },
     {
-      needs: new Set(["v", "c", "c_b", "c_a"]),
-      complete({ v, c, c_b, c_a }) {
-        if (c_a === c_b) return null;
-        return (v * (c - c_a)) / (c_b - c_a);
+      needs: new Set(["v", "c_a", "c", "c_b"]),
+      complete({ v, c_a, c, c_b }) {
+        if (c_b === c_a) return null;
+        return (v * (c_a - c)) / (c_a - c_b);
       },
     },
   ],
 };
+
+const dependantsFor = (name) =>
+  filterObject(
+    (_, formulae) => formulae.some(({ needs }) => needs.has(name)),
+    completions,
+  );
 
 const equation = (inputs) => {
   const reset = () => {
@@ -124,22 +187,39 @@ const equation = (inputs) => {
       filterObject((_, v) => !isEmptyInput(v), inputs),
     );
 
-  const complete = (changedVariableName, depth = 0) => {
-    if (depth > 5) return;
+  const complete = (changedVariableName) => {
+    const initialValues = currentValues();
+    const populatedVariables = new Set(Object.keys(initialValues));
+    const dependants = dependantsFor(changedVariableName);
 
-    const values = currentValues();
-    const populatedVariables = new Set(Object.keys(values));
-    const evaluableDependants = Object.entries(completions)
+    const isEmpty = !populatedVariables.has(changedVariableName);
+    if (isEmpty) {
+      const unsatisfiedDeps = filterObject((_, formulae) => {
+        const otherFormulae = formulae.filter(
+          ({ needs }) => !needs.has(changedVariableName),
+        );
+
+        return !(
+          otherFormulae.length > 0 &&
+          otherFormulae.every(({ needs }) =>
+            needs.isSubsetOf(populatedVariables),
+          )
+        );
+      }, dependants);
+
+      Object.keys(unsatisfiedDeps).forEach((name) => {
+        inputs[name].value = "";
+      });
+
+      return;
+    }
+
+    const evaluableDependants = Object.entries(dependants)
       .map(([name, candidateFormulae]) => {
         const formula =
           candidateFormulae
             .sort((a, b) => a.needs.size - b.needs.size)
-            .find(({ needs }) => {
-              return (
-                needs.has(changedVariableName) &&
-                needs.isSubsetOf(populatedVariables)
-              );
-            }) || null;
+            .find(({ needs }) => needs.isSubsetOf(populatedVariables)) || null;
 
         return {
           name,
@@ -149,18 +229,15 @@ const equation = (inputs) => {
       .filter(({ formula }) => !!formula);
 
     evaluableDependants.forEach(({ name, formula }) => {
+      const values = currentValues();
       const computedValue = formula.complete(values);
-      if (computedValue === null || computedValue < 0) {
+
+      if (computedValue === null) {
         inputs[name].value = "";
         return;
       }
 
-      const currentValue = values[name];
-      const nextValue = round(computedValue);
-      if (approxEqual(currentValue, nextValue)) return;
-
-      inputs[name].value = nextValue;
-      complete(name, depth + 1);
+      inputs[name].value = round(computedValue);
     });
   };
 
